7ea10f407a5285cce1864d71ae889fda65c1de22
Administrator <mune183com@gmail.com>
2020-09-19 23:28:49 +0900

    message

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket = "mune183com-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
